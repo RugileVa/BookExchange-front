ebceaee3d668dc34be42fbfbf6070f8124e16870
Rugile Vasaityte <rugile.vasaityte@gmail.com>
2024-04-20 08:46:45 +0300

    refactoring of home component to suit angular

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  standalone: true,
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css'],
+  imports: [FormsModule, CommonModule],
+  standalone: true,
+  providers: [AuthService]
 })
 export class HomeComponent {
 
+  constructor(private authService: AuthService, private router: Router) { }
+
 }
+
+
+
